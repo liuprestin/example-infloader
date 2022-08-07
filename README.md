f9893f5e5bb198d9114f0bfcc97a1b8197f2364d
Prestin Liu <liuprestin@gmail.com>
2022-08-06 21:06:45 -0700

    update

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Notes
 
 - loading.io
 - fonts.google.com
+- unsplash
+- https://unsplash.com/developers
+
+looks > styling -> than work with api ? 
+
+
+For API access - what parts of your query string do you need? 
---
+++ script.js
@@ -0,0 +1,9 @@
+// Unsplash API 
+// keep keys confidental
+// https://unsplash.com/documentation#get-a-random-photo 
+
+
+const apiUrl = "https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY";
+
+const client_id = "O0zafcf6n0GyCj1MUC0UMkVSzXMWYudenKr6THdNUZI";
+
